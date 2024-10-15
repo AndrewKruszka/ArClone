306394eb995f79cd9fec6266c7a48102b33627c0
AndrewKruszka <andkruszka@gmail.com>
2024-10-15 13:46:14 -0500

    Trying to integrate roboflow

--- threex/threex-arbasecontrols.js
+++ threex/threex-arbasecontrols.js
@@ -1,36 +1,64 @@
 var THREEx = THREEx || {}
 
 THREEx.ArBaseControls = function(object3d){
-	this.id = THREEx.ArBaseControls.id++
+    this.id = THREEx.ArBaseControls.id++
 
-	this.object3d = object3d
-	this.object3d.matrixAutoUpdate = false;
-	this.object3d.visible = false
+    this.object3d = object3d
+    this.object3d.matrixAutoUpdate = false
+    this.object3d.visible = false
 
-	// Events to honor
-	// this.dispatchEvent({ type: 'becameVisible' })
-	// this.dispatchEvent({ type: 'markerVisible' })	// replace markerFound
-	// this.dispatchEvent({ type: 'becameUnVisible' })
+    // Events to honor
+    // this.dispatchEvent({ type: 'becameVisible' })
+    // this.dispatchEvent({ type: 'markerVisible' }) // replace markerFound
+    // this.dispatchEvent({ type: 'becameUnVisible' })
 }
 
 THREEx.ArBaseControls.id = 0
 
-Object.assign( THREEx.ArBaseControls.prototype, THREE.EventDispatcher.prototype );
+Object.assign(THREEx.ArBaseControls.prototype, THREE.EventDispatcher.prototype)
 
 //////////////////////////////////////////////////////////////////////////////
-//		Functions
+//      Functions
 //////////////////////////////////////////////////////////////////////////////
+
 /**
- * error catching function for update()
+ * Function to handle marker updates, to be implemented by subclasses.
  */
 THREEx.ArBaseControls.prototype.update = function(){
-	console.assert(false, 'you need to implement your own update')
+    console.assert(false, 'you need to implement your own update')
 }
 
 /**
- * error catching function for name()
+ * Function to return the name of the control, to be implemented by subclasses.
  */
 THREEx.ArBaseControls.prototype.name = function(){
-	console.assert(false, 'you need to implement your own .name()')
-	return 'Not yet implemented - name()'
+    console.assert(false, 'you need to implement your own .name()')
+    return 'Not yet implemented - name()'
+}
+
+/**
+ * Custom implementation for Roboflow marker detection.
+ * @param {Object} result - Results from Roboflow API, including marker bounding box.
+ */
+THREEx.ArBaseControls.prototype.updateWithRoboflow = function(result){
+    var object3d = this.object3d
+
+    // Mark the object as visible
+    object3d.visible = true
+
+    // Update object's matrix using the bounding box data returned from Roboflow
+    var modelViewMatrix = new THREE.Matrix4().fromArray(result.bbox)
+
+    object3d.matrix.copy(modelViewMatrix)
+    object3d.matrix.decompose(object3d.position, object3d.quaternion, object3d.scale)
+
+    // Trigger a custom event for marker found
+    this.dispatchEvent({ type: 'markerFound' })
+}
+
+/**
+ * Provide a name for the marker. Customize this for Roboflow markers if needed.
+ */
+THREEx.ArBaseControls.prototype.name = function(){
+    return 'RoboflowMarker-' + this.id
 }
